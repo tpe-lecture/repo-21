435f972ee4cdf2829c025571b5ae9f1ce613cfd5
Luca-Laptop <1610927@stud.hs-mannheim.de>
2016-10-24 11:34:47 +0200

    created the constuctors for Nachtelf and Untoter and  fixed the JUnit test

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
@@ -3,19 +3,35 @@ package tpe.oo.constructor;
 /**
  * Nachtelf aus der Allianz.
  */
-public class Nachtelf /* extends Figur */ {
+public class Nachtelf extends Wesen {
 
     /** Standardmäßige Stärke der Fähigkeit Naturwiderstand. */
     public static final int STANDARD_NATURWIDERSTAND = 5;
-    
-    /** Fähigkeit zum Widerstand gegen Naturmagie */
+
+    /** Fähigkeit zum Widerstand gegen Naturmagie. */
     private int naturwiderstand;
-    
-    // Konstruktoren fehlen. Bitte hier einfügen.
-    
+
+    /**
+     *
+     * @param name Name des Nachtelfen
+     * @param naturwiderstand Stärke der Spezielfähigkeit Naturwiderstand
+     */
+    public Nachtelf(String name, int naturwiderstand) {
+        super(name);
+        this.naturwiderstand = naturwiderstand;
+    }
+
+    /**
+     *
+     * @param name Name des Nachtelfen
+     */
+    public Nachtelf(String name) {
+        this(name, STANDARD_NATURWIDERSTAND);
+    }
+
     /**
      * Stärke des Naturwiderstandes.
-     * 
+     *
      * @return the naturwiderstand
      */
     public int getNaturwiderstand() {
