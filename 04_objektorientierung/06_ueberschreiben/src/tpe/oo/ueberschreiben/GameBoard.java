4279c6f71db6ee5fd555174abb20eaac8f19050f
Luca-Laptop <1610927@stud.hs-mannheim.de>
2016-10-31 10:01:07 +0100

    changed the background

--- 04_objektorientierung/06_ueberschreiben/src/tpe/oo/ueberschreiben/GameBoard.java
+++ 04_objektorientierung/06_ueberschreiben/src/tpe/oo/ueberschreiben/GameBoard.java
@@ -3,19 +3,23 @@ package tpe.oo.ueberschreiben;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.util.Random;
 import de.smits_net.games.framework.sprite.Direction;
 
 import de.smits_net.games.framework.board.Board;
+import de.smits_net.games.framework.image.ImageBase;
 
 /**
  * Spielfeld.
  */
+@SuppressWarnings("serial")
 public class GameBoard extends Board {
 
     /** Alien, das durch das Bild läuft. */
-    private Alien alien, alienZwei, alienDrei;
+    private Alien[] aliens = new Alien[8];
+    private Image background;
 
     /**
      * Erzeugt ein neues Board.
@@ -23,31 +27,40 @@ public class GameBoard extends Board {
     public GameBoard() {
         // neues Spielfeld anlegen
         super(10, new Dimension(400, 400), Color.BLACK);
+        // background laden
+        background = ImageBase.loadImage("assets/background");
 
         // Alien initialisieren
-        alien = new Alien(this, new Point(400, 50 + new Random().nextInt(100)));
-        alienZwei = new Alien(this,
-                new Point(300, 50 + new Random().nextInt(100)), Direction.EAST,
-                5);
-        alienDrei = new Alien(this,
-                new Point(200, 50 + new Random().nextInt(100)),
-                Direction.NORTHEAST, 4);
+        for (Alien alien : aliens) {
+            alien = new Alien(this,
+                    new Point(new Random().nextInt(400),
+                            50 + new Random().nextInt(100)),
+                    Direction.WEST, new Random().nextInt(5));
+        }
+
         // Alien soll auf Maus-Klicks reagieren
-        addMouseListener(alien);
-        addMouseListener(alienZwei);
-        addMouseListener(alienDrei);
+        for (Alien alien : aliens) {
+            addMouseListener(alien);
+        }
 
     }
 
     /**
      * Spielfeld neu zeichnen. Wird vom Framework aufgerufen.
      */
+    /**
+     * @param g fdg
+     */
+    protected void drawBackground(Graphics g) {
+        g.drawImage(background, 0, 0, null);
+    }
+
     @Override
     public void drawGame(Graphics g) {
         // Alien zeichnen
-        alien.draw(g, this);
-        alienZwei.draw(g, this);
-        alienDrei.draw(g, this);
+        for (Alien alien : aliens) {
+            alien.draw(g);
+        }
     }
 
     /**
@@ -55,10 +68,21 @@ public class GameBoard extends Board {
      */
     @Override
     public boolean updateGame() {
-        alien.move();
-        alienZwei.move();
-        alienDrei.move();
-        return alien.isVisible() || alienZwei.isVisible()
-                || alienDrei.isVisible();
+        for (Alien alien : aliens) {
+            alien.move();
+        }
+        return isVisible();
+    }
+
+    public boolean isVisible() {
+        for (Alien alien : aliens) {
+            if (alien.isVisible()) {
+                return true;
+            }
+            else if(!alien.isVisible()){
+                return false;
+            }
+        }
+
     }
 }
