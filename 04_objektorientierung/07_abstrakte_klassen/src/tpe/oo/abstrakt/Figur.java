341e5d8e0ec5f43b04ea2853e68f3d098763b944
Luca-Laptop <1610927@stud.hs-mannheim.de>
2016-10-31 10:51:38 +0100

    created the abstract class Figur and change the class Geometr so it uses polymorphie

---
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Figur.java
@@ -0,0 +1,15 @@
+package tpe.oo.abstrakt;
+
+/**
+ * Abstrakte Klasse zur vereinfachung von Kreis und Dreieck.
+ *
+ * @author Luca
+ *
+ */
+public abstract class Figur {
+    /**
+     * Berechnet die Fläche dergeometrischen Figur.
+     * @return gibt die berechnete Fläche zurück
+     */
+    public abstract double flaeche();
+}
