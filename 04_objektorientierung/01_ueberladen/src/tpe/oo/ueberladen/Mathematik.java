4a252a9120b82c2593ab897df9ae8eab2f7d0994
Luca-Laptop <1610927@stud.hs-mannheim.de>
2016-11-02 09:02:55 +0100

    solve task 04/01 by creating the method max and overload it for 3 and 4 Int numbers

--- 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
+++ 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
@@ -8,8 +8,58 @@ public final class Mathematik {
     /**
      * Konstruktor.
      */
-    private Mathematik() {        
+    private Mathematik() {
     }
-    
-    // Bitte fügen Sie Ihre Methoden hier ein
+
+    /**
+     * Bestimmt das größere Element von beiden.
+     *
+     * @param zahlEins erste Zahl die übergeben wird
+     * @param zahlZwei zwete Zahl die übergeben wird
+     * @return die Größte Zahl der beiden
+     */
+    public static int max(int zahlEins, int zahlZwei) {
+        if (zahlEins > zahlZwei) {
+            return zahlEins;
+        }
+        else {
+            return zahlZwei;
+        }
+    }
+
+    /**
+     * Überladene Mtehote für 3 Int zahlen.
+     *
+     * @param zahlEins erste übergebene Zahl
+     * @param zahlZwei zweite übergebene Zahl
+     * @param zahlDrei dritte übergebene Zahl
+     * @return die Größte der drei Zahlen
+     */
+    public static int max(int zahlEins, int zahlZwei, int zahlDrei) {
+        if (max(zahlEins, zahlZwei) > zahlDrei) {
+            return max(zahlEins, zahlZwei);
+        }
+        else {
+            return zahlDrei;
+        }
+    }
+
+    /**
+     * Überldene Methode die für Vier Int Zahlen funktioniert.
+     *
+     * @param zahlEins erste übergebene Zahl
+     * @param zahlZwei zweite übergebene Zahl
+     * @param zahlDrei dritte übergebene Zahl
+     * @param zahlVier vierte übergebene Zahl
+     * @return die Größte der vier Zahlen
+     */
+    public static int max(int zahlEins, int zahlZwei, int zahlDrei,
+            int zahlVier) {
+        if (max(zahlEins, zahlZwei, zahlDrei) > zahlVier) {
+            return max(zahlEins, zahlZwei, zahlDrei);
+        }
+        else {
+            return zahlVier;
+        }
+    }
 }
